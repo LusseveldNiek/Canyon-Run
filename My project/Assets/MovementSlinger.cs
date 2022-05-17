432ae5eb0dfb4095314d7866d6e787e725054de3
LusseveldNiek <97088465@st.deltion.nl>
2022-05-17 11:42:20 +0200

    Obstakel script

--- My project/Assets/MovementSlinger.cs
+++ My project/Assets/MovementSlinger.cs
@@ -7,11 +7,15 @@ public class MovementSlinger : MonoBehaviour
     public Vector3 v;
     public float time;
     public float timeObstakel;
+    public float speed;
+    public bool left;
+    public float timeLeft;
+   
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        left = false;
     }
 
     // Update is called once per frame
@@ -22,8 +26,36 @@ public class MovementSlinger : MonoBehaviour
         time += Time.deltaTime;
         if(time > 1)
         {
-            time = 0;
-            timeObstakel += 1;
+            if(left == false)
+            {
+
+            
+                time = 0;
+                timeObstakel += speed;
+            }
         }
+
+
+        if(timeObstakel > 0.0004)
+        {
+            left = true;
+        
+            
+        }
+
+        if(left == true)
+        {   
+            timeLeft += Time.deltaTime;
+            if(timeLeft > 1)
+            {
+                timeLeft = 0;
+                timeObstakel -= speed;
+                if(timeObstakel < -0.004)
+                {
+                    left = false;
+                }
+            }
+        }
+        
     }
 }
