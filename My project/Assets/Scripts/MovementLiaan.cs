32d5a86305fb08c878d163ab1dc5b7033940299d
LusseveldNiek <97088465@st.deltion.nl>
2022-05-19 12:35:43 +0200

    Slinger

---
+++ My project/Assets/Scripts/MovementLiaan.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementLiaan : MonoBehaviour
+{
+    public float speed;
+    public float time;
+    public float timeRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time < 3)
+        {
+             transform.Rotate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if(time > 3)
+        {
+            transform.Rotate(Vector3.left * speed * Time.deltaTime);
+            timeRight += Time.deltaTime;
+            if(timeRight > 5)
+            {
+                timeRight = 0;
+                time = 0;
+            }
+        }
+    }
+}
