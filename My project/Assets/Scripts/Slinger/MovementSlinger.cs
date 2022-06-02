39b8ff60936d0585aa3d354166492540fde2ec98
LusseveldNiek <97088465@st.deltion.nl>
2022-06-02 20:38:13 +0200

    Lighting

--- My project/Assets/Scripts/Slinger/MovementSlinger.cs
+++ My project/Assets/Scripts/Slinger/MovementSlinger.cs
@@ -6,33 +6,46 @@ public class MovementSlinger : MonoBehaviour
 {
     public float speed;
     public float time;
-    public float timeRight;
+    public bool reset;
     
    
    
     // Start is called before the first frame update
     void Start()
     {
-       
+       reset = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed += Time.deltaTime * 14;
         time += Time.deltaTime;
-        if(time < 3)
+        if(time < 7)
         {
+             
              transform.Rotate(Vector3.right * speed * Time.deltaTime);
         }
+        
+        if(time > 7)
+        {
+            if(reset == true)
+            {
+                speed = 0;
+                reset = false;
+            }
+            
+           
+        }
 
-        if(time > 3)
+        if(reset == false)
         {
             transform.Rotate(Vector3.left * speed * Time.deltaTime);
-            timeRight += Time.deltaTime;
-            if(timeRight > 3)
+            if(time > 14)
             {
-                timeRight = 0;
                 time = 0;
+                speed = 0;
+                reset = true;
             }
         }
 
